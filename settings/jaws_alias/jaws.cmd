2b8a541abf5e0ff2894d1caa3db51aa2b1c22f37
Dominic Oram <dominic.oram@stfc.ac.uk>
2019-09-03 14:16:06 +0100

    Added jaws alias

---
+++ settings/jaws_alias/jaws.cmd
@@ -0,0 +1,3 @@
+# Load records for an aliased full jawset
+
+$(IFIOC_TWINCAT_01=#) dbLoadRecords("$(JAWS)/db/jaws_alias.db","P=$(MYPVPREFIX)MOT:,JAWS=JAWS1:,mXN=MTR0101,mXS=MTR0102,mXW=MTR0103,mXE=MTR0104,mVGAP=MTR0105,mVCENT=MTR0106,mHGAP=MTR0107,mHCENT=MTR0108,EGU=mm")
