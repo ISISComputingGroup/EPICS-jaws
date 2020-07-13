478ceab358ad4237ebad4d6285c9e0643a4d9e77
Tom Willemsen <tomwillemsen1995@gmail.com>
2020-07-13 14:32:33 +0100

    Remove broken comment

--- settings/jaws_galilmul/jaws.cmd
+++ settings/jaws_galilmul/jaws.cmd
@@ -1,3 +1,1 @@
-REM Load records for full jawset that is split over two separate motor controllers
-
 $(IFIOC_GALILMUL_01=#) dbLoadRecords("$(JAWS)/db/jaws.db","P=$(MYPVPREFIX)MOT:,JAWS=JAWS1:,mXN=MTR0101,mXS=MTR0102,mXW=MTR0103,mXE=MTR0201")
