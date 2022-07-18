1c4bb0159daec9cbcc75663bd276af5b9f843c83
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2022-07-18 11:06:04 +0100

    Remove feedback from low level

--- settings/jaws_full/jaws.cmd
+++ settings/jaws_full/jaws.cmd
@@ -1,3 +1,3 @@
-REM Load records for standard full Jawset
+# Load records for standard full Jawset
 
 $(IFIOC_GALIL_01=#) dbLoadRecords("$(JAWS)/db/jaws.db","P=$(MYPVPREFIX)MOT:,JAWS=JAWS1:,mXN=MTR0101,mXS=MTR0102,mXW=MTR0103,mXE=MTR0104")
--- settings/jaws_vertical_only/jaws.cmd
+++ settings/jaws_vertical_only/jaws.cmd
@@ -1,3 +1,3 @@
-REM Load records for Jawset that only has vertical blades only
+# Load records for Jawset that only has vertical blades only
 
 $(IFIOC_GALIL_01) dbLoadRecords("$(JAWS)/db/jaws_vertical.db","P=$(MYPVPREFIX)MOT:,JAWS=JAWS1:,mXN=MTR0101,mXS=MTR0102")
