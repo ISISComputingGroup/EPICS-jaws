2227db4bcc3d1ebec663c509963704de740b23df
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2022-11-04 22:44:41 +0000

    Update jaws.cmd

--- settings/jaws_vertical_only/jaws.cmd
+++ settings/jaws_vertical_only/jaws.cmd
@@ -1,3 +1,3 @@
 # Load records for Jawset that only has vertical blades only
 
-$(IFIOC_GALIL_01) dbLoadRecords("$(JAWS)/db/jaws_vertical.db","P=$(MYPVPREFIX)MOT:,JAWS=JAWS1:,mXN=MTR0101,mXS=MTR0102")
+$(IFIOC_GALIL_01) dbLoadRecords("$(JAWS)/db/jaws_vertical.db","P=$(MYPVPREFIX)MOT:,JAWS=JAWS1:,mXN=MTR0101,mXS=MTR0102,IFINIT_FROM_AS=$(IFINIT_JAWS_FROM_AS=#),IFNOTINIT_FROM_AS=$(IFNOTINIT_JAWS_FROM_AS=)")
